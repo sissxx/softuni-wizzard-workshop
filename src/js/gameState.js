e028716c0aa982a196e0cf6d226bb381a91094b6
sissxx <siss99.sh@gmail.com>
2023-03-04 17:45:17 +0200

    Create wizard with initial position on start

--- src/js/gameState.js
+++ src/js/gameState.js
@@ -1,7 +1,13 @@
-function initState(){
-    const state = {
-        player : 'Maria'
-    };
+function initState() {
+  const state = {
+    player: "Maria",
+    wizard: {
+      width: 82,
+      height: 100,
+      startX: Math.floor(Math.random() * 1000),
+      startY: Math.floor(Math.random() * 500),
+    },
+  };
 
-    return state;
+  return state;
 }
