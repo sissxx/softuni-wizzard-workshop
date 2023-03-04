8f3ad8fee9d2e739a5b0ba49acf93ed91f7db989
sissxx <siss99.sh@gmail.com>
2023-03-04 18:10:06 +0200

    Add move to rigth

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -5,7 +5,17 @@ function start(state, game) {
 }
 
 function gameLoop(state, game) {
-    // console.log(state.player);
-    console.log(state.keys);
-    window.requestAnimationFrame(gameLoop.bind(null, state, game));
+  // Move wizard
+  const { wizard } = state;
+  const { wizardElement } = game;
+
+  if (state.keys.KeyD) {
+    wizard.posX += 10;
+  }
+
+  // Render wizard
+  wizardElement.style.left = wizard.posX + "px";
+
+
+  window.requestAnimationFrame(gameLoop.bind(null, state, game));
 }
--- src/js/gameState.js
+++ src/js/gameState.js
@@ -1,13 +1,16 @@
 function initState() {
+  let startX = Math.floor(Math.random() * 1000);
+  let startY = Math.floor(Math.random() * 500);
+
   const state = {
     player: "Maria",
     wizard: {
       width: 82,
       height: 100,
-      startX: Math.floor(Math.random() * 1000),
-      startY: Math.floor(Math.random() * 500),
+      posX: startX,
+      posY: startY,
     },
-    keys: {}
+    keys: {},
   };
 
   return state;
